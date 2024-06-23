b59825ad2a2e51482c2f93533b255ba05f994731
Redbutcher <johndoe@example.compaul34913@gmail.com>
2024-06-23 14:12:13 +0100

    swap variables excercise complete

---
+++ 14.1 Introdution to Javascript ES6 - Javascript Variables Exercise/assets/script.js
@@ -0,0 +1,17 @@
+function swapVariables() {
+
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+
+    document.getElementById('outputMessage').textContent = "Output";
+
+    document.getElementById('chosenMessage').textContent = "Using the numbers you have chosen, the variables have been set as follows:";
+
+    document.getElementById('outputA').textContent = "a = " + num1 + " b = " + num2;
+
+    [num1, num2] = [num2, num1];
+   
+    document.getElementById('swapMessage').textContent = "By switching the variables using arrays [a, b] = [b, a], we can see that the variables have now switched places:";
+
+    document.getElementById('outputB').textContent = "a = " + num1 + " b = " + num2;
+}
