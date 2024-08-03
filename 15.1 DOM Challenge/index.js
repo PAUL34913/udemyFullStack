37eeb5641f2dd07bfbdfcf50c6d7df32440867b9
Redbutcher <johndoe@example.compaul34913@gmail.com>
2024-08-03 10:50:05 +0100

    DOM

---
+++ 15.1 DOM Challenge/index.js
@@ -0,0 +1,3 @@
+document.querySelectorAll("ul .list")[2].innerHTML = "Paul Eagle"; /* better option from AI */
+
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Angela"; /* course option */
